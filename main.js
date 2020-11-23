e8671aefc5bffb394fd968399b3b87313d9d90dc
yuu-hiraguchi <74848357+yuu-hiraguchi@users.noreply.github.com>
2020-11-23 11:40:06 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+main();
+
+async function main() {
+  var gpioAccess = await navigator.requestGPIOAccess();
+  var sensor = document.getElementById("sensor");
+  var dPort = gpioAccess.ports.get(12);
+  await dPort.export("in");
+  dPort.onchange = function(v) {
+    if (v === 1) {
+      sensor.innerHTML = "ON";
+    } else {
+      sensor.innerHTML = "OFF";
+    }
+  };
+}
